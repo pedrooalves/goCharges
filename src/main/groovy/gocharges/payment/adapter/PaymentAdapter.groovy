d9ebdc3a48246841c39452222e1aec77fc807545
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-05-23 13:46:39 -0300

    Atualizando adapter

--- src/main/groovy/gocharges/payment/adapter/PaymentAdapter.groovy
+++ src/main/groovy/gocharges/payment/adapter/PaymentAdapter.groovy
@@ -2,14 +2,14 @@ package gocharges.payment.adapter
 
 class PaymentAdapter {
 
-    String payerId
+    String payerCpfCnpj
     String billingType
     String status
     String dueDate
     String value
 
     public PaymentAdapter(Map params) {
-        this.payerId = params.payerId
+        this.payerCpfCnpj = params.payerCpfCnpj
         this.billingType = params.billingType
         this.status = params.status
         this.dueDate  = params.dueDate
