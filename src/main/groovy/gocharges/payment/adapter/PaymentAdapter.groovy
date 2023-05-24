08bcbefaa1e983a08baf7a7eb1231083dff0ef49
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-05-24 12:26:14 -0300

    Alterando tipo da variavel status para PaymentStatus

--- src/main/groovy/gocharges/payment/adapter/PaymentAdapter.groovy
+++ src/main/groovy/gocharges/payment/adapter/PaymentAdapter.groovy
@@ -1,17 +1,18 @@
 package gocharges.payment.adapter
 
+import gocharges.payment.enums.PaymentStatus
+
 class PaymentAdapter {
 
     String payerCpfCnpj
     String billingType
-    String status
+    PaymentStatus status
     String dueDate
     String value
 
     public PaymentAdapter(Map params) {
         this.payerCpfCnpj = params.payerCpfCnpj
         this.billingType = params.billingType
-        this.status = params.status
         this.dueDate  = params.dueDate
         this.value = params.value
     }
