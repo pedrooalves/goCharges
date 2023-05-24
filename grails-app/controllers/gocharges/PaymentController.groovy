3cfc88538f6d57283d7437a1e8a5effc4b3cdc07
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-05-24 09:49:34 -0300

    Padronizando o código

--- grails-app/controllers/gocharges/PaymentController.groovy
+++ grails-app/controllers/gocharges/PaymentController.groovy
@@ -14,9 +14,9 @@ class PaymentController {
         if(chainModel) {
             Map validation = chainModel.validation
             showNewPaymentForm = chainModel.showNewPaymentForm
-            render(view: "index", model: [payments:payments, validation: validation, showNewPaymentForm: showNewPaymentForm])
+            render(view: "index", model: [payments: payments, validation: validation, showNewPaymentForm: showNewPaymentForm])
         } else {
-            render(view: "index", model: [payments:payments, showNewPaymentForm: showNewPaymentForm])
+            render(view: "index", model: [payments: payments, showNewPaymentForm: showNewPaymentForm])
         }
     }
 
@@ -25,11 +25,11 @@ class PaymentController {
             PaymentAdapter paymentAdapter = new PaymentAdapter(params)
             Payment payment = paymentService.save(paymentAdapter)
 
-            Map validation = [success:true, message:"Cobrança criada com sucesso", type:"save"]
-            chain(action: "index", model:[validation:validation, showNewPaymentForm: false])
+            Map validation = [success: true, message: "Cobrança criada com sucesso", type: "save"]
+            chain(action: "index", model: [validation: validation, showNewPaymentForm: false])
         } catch(BusinessException e) {
-            Map validation = [success:false, message:e.getMessage(), type:"save"]
-            chain(action: "index", model: [validation:validation, showNewPaymentForm: true])
+            Map validation = [success: false, message: e.getMessage(), type: "save"]
+            chain(action: "index", model: [validation: validation, showNewPaymentForm: true])
         }
     }
 
@@ -38,7 +38,7 @@ class PaymentController {
         Payment payment = paymentService.findById(id)
         Payer payer = paymentService.findPayerById(payment.payer.id)
 
-        render(view: "edit", model: [payment : payment, payer:payer])
+        render(view: "edit", model: [payment : payment, payer: payer])
     }
 
     public update() {
@@ -49,10 +49,10 @@ class PaymentController {
             paymentService.update(id, adapter)
 
             Map validation = [success:true, message:"Cobrança editada com sucesso", type:"update"]
-            chain(action: "index", model:[validation:validation, showNewPaymentForm: false])
+            chain(action: "index", model: [validation: validation, showNewPaymentForm: false])
         }catch(BusinessException exception){
             Map validation = [success:false, message:exception.getMessage(), type:"save"]
-            chain(action: "index", model: [validation:validation, showNewPaymentForm: false])
+            chain(action: "index", model: [validation: validation, showNewPaymentForm: false])
         }
     }
 
