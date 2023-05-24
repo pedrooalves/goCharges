fed794846b17de0e19d437d55f18c865c6b308f5
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-05-24 16:20:56 -0300

    CustomerRepository criado

---
+++ src/main/groovy/gocharges/customer/CustomerRepository.groovy
@@ -0,0 +1,28 @@
+package gocharges.customer
+
+import gocharges.Customer
+import grails.gorm.DetachedCriteria
+
+class CustomerRepository {
+
+    public static DetachedCriteria<Customer> query(Map search) {
+        DetachedCriteria<Customer> query = Customer.where {
+            if(search.containsKey("cpfCnpj")) {
+                eq("cpfCnpj", search.cpfCnpj)
+            }
+
+            if(!Boolean.valueOf(search.includeDeleted)) {
+                eq("deleted", false)
+            }
+
+            if(search.containsKey("email")) {
+                eq("email", search.email)
+            }
+
+            if(search.containsKey("id")) {
+                eq("id", search.id)
+            }
+        }
+        return query
+    }
+}
