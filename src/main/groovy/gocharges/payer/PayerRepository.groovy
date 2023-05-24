c93278ba192e41c4bb01be2ea3d568775132709b
Pedro Alves <pedro.alves@asaas.com.br>
2023-05-24 15:45:38 -0300

    Criando repository da Payer

---
+++ src/main/groovy/gocharges/payer/PayerRepository.groovy
@@ -0,0 +1,21 @@
+package gocharges.payer
+
+import gocharges.Payer
+import grails.gorm.DetachedCriteria
+
+class PayerRepository {
+    public static DetachedCriteria<Payer> query(Map search) {
+        DetachedCriteria<Payer> query = Payer.where {
+            if(search.containsKey("cpfCnpj")) {
+                println("cpfCnpj")
+                eq("cpfCnpj", search.cpfCnpj)
+            }
+
+            if(!Boolean.valueOf(search.includeDeleted)) {
+                println("includeDeleted")
+                eq("deleted", false)
+            }
+        }
+        return query
+    }
+}
