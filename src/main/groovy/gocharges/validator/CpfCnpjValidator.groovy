be58aa69e6ebc79d42f0f5eb300038bb0d411aab
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-05-26 10:32:07 -0300

    Criando variável cpfCnpjLength

--- src/main/groovy/gocharges/validator/CpfCnpjValidator.groovy
+++ src/main/groovy/gocharges/validator/CpfCnpjValidator.groovy
@@ -5,8 +5,9 @@ import gocharges.exception.BusinessException
 class CpfCnpjValidator {
 
      public static void validate(String cpfCnpj) {
-        if(cpfCnpj.length() != 11 && cpfCnpj.length() != 14){
-            throw new BusinessException("Informe um tamanho de CPF / CNPJ correto.")
-        }
+         Integer cpfCnpjLength = cpfCnpj.length()
+         if(cpfCnpjLength != 11 && cpfCnpjLength != 14){
+             throw new BusinessException("Informe um tamanho de CPF / CNPJ correto.")
+         }
     }
 }
