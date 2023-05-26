fca4d5f5a78e7a08bc69743c59e75a8417b7c1cc
Pedro Alves <pedro.alves@asaas.com.br>
2023-05-26 13:06:50 -0300

    Adicionando dependência e criando job

---
+++ grails-app/jobs/gocharges/PaymentOverdueJob.groovy
@@ -0,0 +1,23 @@
+package gocharges
+
+import gocharges.payment.PaymentRepository
+
+class PaymentOverdueJob {
+
+    static triggers = {
+        cron name: 'cronTrigger', cronExpression: '0 05 13 1/1 * ? *'
+    }
+
+    def execute() {
+
+        Date today = new Date()
+        List<Payment> paymentsOverdue = PaymentRepository.query([today: today]).list()
+
+        println(paymentsOverdue)
+
+        for(Payment payment : paymentsOverdue) {
+            PaymentService.setOverdue(payment.id)
+        }
+
+    }
+}
