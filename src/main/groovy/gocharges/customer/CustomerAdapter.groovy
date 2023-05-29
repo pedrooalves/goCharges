bfa9636f8f28fe0d955b3a333e8bcfece181f603
Pedro Alves <pedro.alves@asaas.com.br>
2023-05-29 15:13:34 -0300

    Refatoração CustomerAdapter

--- src/main/groovy/gocharges/customer/CustomerAdapter.groovy
+++ src/main/groovy/gocharges/customer/CustomerAdapter.groovy
@@ -3,14 +3,12 @@ package gocharges.customer
 class CustomerAdapter {
 
     String name
-    String email
     String mobilePhone
     String cpfCnpj
     String address
 
     CustomerAdapter(Map params) {
         this.name = params.name
-        this.email = params.email
         this.mobilePhone = params.mobilePhone
         this.cpfCnpj = params.cpfCnpj
         this.address = params.address
