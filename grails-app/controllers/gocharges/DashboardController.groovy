f69a126c225ce4b0266d0ca0d08c09baef5b0dfb
Pedro Alves <pedro.alves@asaas.com.br>
2023-05-31 08:14:16 -0300

    Início do método index do controller e tela index

--- grails-app/controllers/gocharges/DashboardController.groovy
+++ grails-app/controllers/gocharges/DashboardController.groovy
@@ -1,21 +1,30 @@
 package gocharges
 
 import gocharges.auth.User
+import gocharges.payment.enums.PaymentStatus
 import grails.plugin.springsecurity.SpringSecurityService
 
 class DashboardController {
 
     SpringSecurityService springSecurityService
+    PayerService payerService
+    PaymentService paymentService
 
     def index() {
-
         User currentUser = springSecurityService.loadCurrentUser()
-
 
         if(!currentUser.customer.name) {
             redirect(controller: "customer", action: "create")
         }
 
-        render(view: "index")
+        Integer payerCount = payerService.payerCount()
+
+        Map paymentsByStatus = [
+                pending : paymentService.getPaymentsByStatus(PaymentStatus.PENDING),
+                overdue : paymentService.getPaymentsByStatus(PaymentStatus.OVERDUE),
+                received : paymentService.getPaymentsByStatus(PaymentStatus.RECEIVED)
+        ]
+
+        render(view: "index", model: [payerCount: payerCount, paymentsByStatus : paymentsByStatus])
     }
 }
--- grails-app/views/dashboard/index.gsp
+++ grails-app/views/dashboard/index.gsp
@@ -6,5 +6,28 @@
 </head>
 <body>
     DASHBOARD
+
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <h2 class="font-weight-bold">Clientes</h2>
+        </div>
+        <div class="card-body d-flex justify-content-center align-items-center" style="height: 12rem;">
+            <p class="text-center" style="font-size: 5rem;">${payerCount}</p>
+        </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <h2 class="font-weight-bold">Cobranças</h2>
+        </div>
+
+        <div class="card-body d-flex justify-content-center align-items-center" style="height: 6rem;">
+            <p class="text-center" style="font-size: 3rem;">${payerCount}</p>
+        </div>
+        <div class="card-body d-flex justify-content-center align-items-center" style="height: 6rem;">
+            <p class="text-center" style="font-size: 3rem;">${payerCount}</p>
+        </div>
+    </div>
+
 </body>
 </html>
