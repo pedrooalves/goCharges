e625d51b266b0621877ee2ca9b5d72bbdc3c1060
Pedro Alves <pedro.alves@asaas.com.br>
2023-05-31 13:55:12 -0300

    Encorpando validação de CPF e CNPJ

---
+++ grails-app/utils/shared/CpfCnpjUtils.groovy
@@ -0,0 +1,9 @@
+package shared
+
+public class CpfCnpjUtils {
+    public static String removeNonNumeric(String text) {
+        if (text == null) return null
+
+        return text?.replaceAll("\\D+","")
+    }
+}
