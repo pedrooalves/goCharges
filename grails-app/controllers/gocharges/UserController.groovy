f49f0597662a3533a639618c69e687155014ea2f
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-06-01 15:50:45 -0300

    Padronizando

--- grails-app/controllers/gocharges/UserController.groovy
+++ grails-app/controllers/gocharges/UserController.groovy
@@ -32,18 +32,18 @@ class UserController {
             UserAdapter userAdapter = new UserAdapter(params)
             userService.save(userAdapter)
 
-            Map validation = [success:true, message:"Conta criada com sucesso", type:"save"]
-            chain(action: "signUp", model:[validation:validation])
-        } catch(BusinessException e) {
-            Map validation = [success:false, message:e.getMessage(), type:"save"]
-            chain(action: "signUp", model: [validation:validation])
+            Map validation = [success: true, message: "Conta criada com sucesso", type: "save"]
+            chain(action: "signUp", model: [validation: validation])
+        } catch (BusinessException businessException) {
+            Map validation = [success: false, message: businessException.getMessage(), type: "save"]
+            chain(action: "signUp", model: [validation: validation])
         }
     }
 
     public myAccount() {
         User user = springSecurityService.getCurrentUser()
 
-        if(chainModel) {
+        if (chainModel) {
             Map validation = chainModel.validation
             render(view: "myaccount", model: [user: user, validation: validation])
         } else {
@@ -60,8 +60,8 @@ class UserController {
             Map validation = [success: true, message: "Informações salvas com sucesso", type: "update"]
             chain(action: "myAccount", model: [validation: validation])
         } catch (BusinessException businessException) {
-            Map validation = [success:false, message:businessException.getMessage(), type:"update"]
-            chain(action: "myAccount", model: [validation:validation])
+            Map validation = [success: false, message: businessException.getMessage(), type: "update"]
+            chain(action: "myAccount", model: [validation: validation])
         }
     }
 }
