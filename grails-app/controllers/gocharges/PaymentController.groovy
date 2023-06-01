a4806972194beafd0a5f36bbe8a23f3320bafd39
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-06-01 13:23:17 -0300

    Retirando injeção de dependência

--- grails-app/controllers/gocharges/PaymentController.groovy
+++ grails-app/controllers/gocharges/PaymentController.groovy
@@ -11,8 +11,9 @@ class PaymentController extends BaseController {
     PayerService payerService
 
     public Map index() {
-        List<Payment> paymentList = paymentService.list()
-        List<Payer> payerList = payerService.list(getCurrentCustomer())
+        Customer customer = getCurrentCustomer()
+        List<Payment> paymentList = paymentService.list(params, customer)
+        List<Payer> payerList = payerService.list(customer)
         Boolean showNewPaymentForm = false
 
         if (chainModel) {
@@ -27,7 +28,7 @@ class PaymentController extends BaseController {
     public Map save() {
         try {
             PaymentAdapter paymentAdapter = new PaymentAdapter(params)
-            Payment payment = paymentService.save(paymentAdapter)
+            Payment payment = paymentService.save(paymentAdapter, getCurrentCustomer())
 
             Map validation = [success: true, message: "Cobrança criada com sucesso", type: "save"]
             chain(action: "index", model: [validation: validation, showNewPaymentForm: false])
@@ -49,7 +50,7 @@ class PaymentController extends BaseController {
             PaymentAdapter adapter = new PaymentAdapter(params)
             Long id = Long.parseLong(params.id)
 
-            paymentService.update(id, adapter)
+            paymentService.update(id, adapter, getCurrentCustomer())
 
             Map validation = [success: true, message: "Cobrança editada com sucesso", type: "update"]
             chain(action: "index", model: [validation: validation, showNewPaymentForm: false])
@@ -65,7 +66,7 @@ class PaymentController extends BaseController {
 
     public Map delete() {
         Long id = Long.parseLong(params.id)
-        paymentService.delete(id)
+        paymentService.delete(id, getCurrentCustomer())
 
         Map validation = [success: true, message: "Cobrança excluída com sucesso", type: "delete"]
         redirect(view: "index", model: [validation: validation])
--- grails-app/services/gocharges/PaymentService.groovy
+++ grails-app/services/gocharges/PaymentService.groovy
@@ -5,33 +5,26 @@ import gocharges.payer.PayerRepository
 import gocharges.exception.BusinessException
 import gocharges.payment.adapter.PaymentAdapter
 import grails.gorm.transactions.Transactional
-import grails.plugin.springsecurity.SpringSecurityService
 
 @Transactional
 class PaymentService {
 
-    SpringSecurityService springSecurityService
-
-    public Payment save(PaymentAdapter adapter) {
+    public Payment save(PaymentAdapter adapter, Customer customer) {
         Payment payment = new Payment()
-        Customer customer = springSecurityService.getCurrentUser().customer
         payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj, customer: customer]).get()
         payment.billingType = adapter.billingType
         payment.dueDate = adapter.dueDate
         payment.value = adapter.value
         payment.customer = customer
 
-        payment.save(failOnError: true)
-        return payment
+        return payment.save(failOnError: true)
     }
 
-    public List<Payment> list() {
-        Customer customer = springSecurityService.getCurrentUser().customer
-        return PaymentRepository.query([includeDeleted: false, customer: customer]).list()
+    public List<Payment> list(Map params, Customer customer) {
+        return PaymentRepository.query(params + [customer: customer]).list()
     }
 
-    public Payment update(Long id, PaymentAdapter adapter) {
-        Customer customer = springSecurityService.getCurrentUser().customer
+    public Payment update(Long id, PaymentAdapter adapter, Customer customer) {
         Payment payment = PaymentRepository.query([id: id, customer: customer]).get()
         payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj, customer: customer]).get()
         payment.billingType = adapter.billingType
@@ -40,13 +33,10 @@ class PaymentService {
 
         if (!payment.payer) throw new BusinessException("Pagador não encontrado")
 
-        payment.save(failOnError: true)
-
-        return payment
+        return payment.save(failOnError: true)
     }
 
-    public void delete(Long id) {
-        Customer customer = springSecurityService.getCurrentUser().customer
+    public void delete(Long id, Customer customer) {
         Payment payment = PaymentRepository.query([id: id, customer: customer]).get()
 
         if (!payment) throw new BusinessException("Cobrança não encontrada")
