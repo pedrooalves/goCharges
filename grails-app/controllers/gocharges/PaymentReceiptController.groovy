f5886ced4bba90f8533ebb6b7f5facc79fa42644
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-06 14:29:13 -0300

    Implementação do Controller de PaymentReceipt

--- grails-app/controllers/gocharges/PaymentReceiptController.groovy
+++ grails-app/controllers/gocharges/PaymentReceiptController.groovy
@@ -1,4 +1,14 @@
 package gocharges
 
-class PaymentReceiptController {
+import gocharges.controller.base.BaseController
+
+class PaymentReceiptController extends BaseController {
+
+    PaymentReceiptService paymentReceiptService
+
+    def index() {
+        Map info = paymentReceiptService.create(params.publicId, getCurrentCustomer())
+
+        render(view: "index", params: [publidId: params.publicId], model: [info: info])
+    }
 }
