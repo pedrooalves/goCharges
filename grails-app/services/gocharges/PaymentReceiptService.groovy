047031e5a232a7730d976a4b3a49ec1a1ea30811
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-06 14:29:48 -0300

    Implementação do Service de PaymentReceipt

--- grails-app/services/gocharges/PaymentReceiptService.groovy
+++ grails-app/services/gocharges/PaymentReceiptService.groovy
@@ -1,4 +1,17 @@
 package gocharges
 
+import gocharges.payment.PaymentRepository
+import grails.gorm.transactions.Transactional
+
+@Transactional
 class PaymentReceiptService {
+
+    public Map create(String publicId, Customer customer) {
+        Map info = [:]
+        info.customer = customer
+        info.payment = PaymentRepository.query([publicId: publicId, customer: customer]).get()
+        info.payer = info.payment.payer
+
+        return info
+    }
 }
