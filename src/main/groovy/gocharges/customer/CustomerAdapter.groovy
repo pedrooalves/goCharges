fd820ce42dfb2b0ddcd493db3e415cdb00db9559
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-06-06 13:37:44 -0300

    BasePersonAdapter

--- src/main/groovy/gocharges/customer/CustomerAdapter.groovy
+++ src/main/groovy/gocharges/customer/CustomerAdapter.groovy
@@ -1,32 +1,10 @@
 package gocharges.customer
 
-import shared.Utils
+import gocharges.adapter.base.BasePersonAdapter
 
-class CustomerAdapter {
+class CustomerAdapter extends BasePersonAdapter {
 
-    String name
-    String email
-    String mobilePhone
-    String cpfCnpj
-    String postalCode
-    String address
-    String addressNumber
-    String complement
-    String province
-    String city
-    String state
-
-    CustomerAdapter(Map params) {
-        this.name = params.name
-        this.email = params.email
-        this.mobilePhone = Utils.removeNonNumeric(params.mobilePhone)
-        this.cpfCnpj = Utils.removeNonNumeric(params.cpfCnpj)
-        this.postalCode = Utils.removeNonNumeric(params.postalCode)
-        this.address = params.address
-        this.addressNumber = params.addressNumber
-        this.complement = params.complement
-        this.province = params.province
-        this.city = params.city
-        this.state = params.state
+    public CustomerAdapter(Map params) {
+        super(params)
     }
 }
--- src/main/groovy/gocharges/payer/adapter/PayerAdapter.groovy
+++ src/main/groovy/gocharges/payer/adapter/PayerAdapter.groovy
@@ -1,33 +1,10 @@
 package gocharges.payer.adapter
 
-import gocharges.Payer
-import shared.Utils
+import gocharges.adapter.base.BasePersonAdapter
 
-class PayerAdapter {
-
-    String name
-    String email
-    String mobilePhone
-    String cpfCnpj
-    String postalCode
-    String address
-    String addressNumber
-    String complement
-    String province
-    String city
-    String state
+class PayerAdapter extends BasePersonAdapter {
 
     public PayerAdapter(Map params) {
-        this.name = params.get("name")
-        this.email = params.email
-        this.mobilePhone = Utils.removeNonNumeric(params.mobilePhone)
-        this.cpfCnpj = Utils.removeNonNumeric(params.cpfCnpj)
-        this.postalCode = Utils.removeNonNumeric(params.postalCode)
-        this.address = params.address
-        this.addressNumber = params.addressNumber
-        this.complement = params.complement
-        this.province = params.province
-        this.city = params.city
-        this.state = params.state
+        super(params)
     }
 }
