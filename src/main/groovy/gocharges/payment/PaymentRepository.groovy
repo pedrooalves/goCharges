d7283ee351372f4a983b1c8352942cf0c5ef9890
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-07 11:38:17 -0300

    Query de busca por publicId

--- src/main/groovy/gocharges/payment/PaymentRepository.groovy
+++ src/main/groovy/gocharges/payment/PaymentRepository.groovy
@@ -7,33 +7,33 @@ class PaymentRepository {
 
     public static DetachedCriteria<Payment> query(Map search) {
         DetachedCriteria<Payment> query = Payment.where {
-            if(!Boolean.valueOf(search.ignoreCustomer) && !search.containsKey("customer")) {
+            if (!Boolean.valueOf(search.ignoreCustomer) && !search.containsKey("customer")) {
                 throw new RuntimeException("O atributo customer é obrigatório para executar a consulta.")
             }
 
             if (Boolean.valueOf(search.deletedOnly)) {
                 eq("deleted", true)
-            } else if(!Boolean.valueOf(search.includeDeleted)) {
+            } else if (!Boolean.valueOf(search.includeDeleted)) {
                 eq("deleted", false)
             }
 
-            if(search.containsKey("id")) {
+            if (search.containsKey("id")) {
                 eq("id", search.id)
             }
 
-            if(search.containsKey("publicId")) {
-                eq("publicId", search.publicId)
-            }
-
-            if(search.containsKey("dueDate[le]")) {
+            if (search.containsKey("dueDate[le]")) {
                 le("dueDate", search."dueDate[le]")
             }
 
-            if(search.containsKey("status")) {
+            if (search.containsKey("status")) {
                 eq("status", search.status)
             }
 
-            if(search.containsKey("customer")) {
+            if (search.containsKey("publicId")) {
+                eq("publicId", search.publicId)
+            }
+
+            if (search.containsKey("customer")) {
                 eq("customer", search.customer)
             }
         }
