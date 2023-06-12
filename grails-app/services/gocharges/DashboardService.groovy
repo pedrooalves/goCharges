bcdcc0f65714ac8240c5b3c4ea6b10d7b3cc9e92
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-12 10:24:42 -0300

    Correção uso de variavel

--- grails-app/services/gocharges/DashboardService.groovy
+++ grails-app/services/gocharges/DashboardService.groovy
@@ -11,9 +11,9 @@ class DashboardService {
     public Map info(Customer customer) {
         Map info = [:]
         info.payerCount = PayerRepository.query([customer: customer]).count()
-        info.pendingPaymentCount = PaymentRepository.query([status: PaymentStatus.PENDING, customer: userCustomer]).count()
-        info.overduePaymentCount = PaymentRepository.query([status: PaymentStatus.OVERDUE, customer: userCustomer]).count()
-        info.receivedPaymentCount = PaymentRepository.query([status: PaymentStatus.RECEIVED, customer: userCustomer]).count()
+        info.pendingPaymentCount = PaymentRepository.query([status: PaymentStatus.PENDING, customer: customer]).count()
+        info.overduePaymentCount = PaymentRepository.query([status: PaymentStatus.OVERDUE, customer: customer]).count()
+        info.receivedPaymentCount = PaymentRepository.query([status: PaymentStatus.RECEIVED, customer: customer]).count()
 
         return info
     }
