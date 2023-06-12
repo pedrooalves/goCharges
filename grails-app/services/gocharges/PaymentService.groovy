f7f0e1eeed4a24f5928a3be6900f2651d4bcf91b
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-06-12 13:14:14 -0300

    Usando utils ao jogar exception

--- grails-app/services/gocharges/PaymentService.groovy
+++ grails-app/services/gocharges/PaymentService.groovy
@@ -33,7 +33,7 @@ class PaymentService {
         payment.dueDate = adapter.dueDate
         payment.value = adapter.value
 
-        if (!payment.payer) throw new BusinessException("Pagador não encontrado")
+        if (!payment.payer) throw new BusinessException(Utils.getMessageProperty("default.not.found.message", "Pagador"))
 
         return payment.save(failOnError: true)
     }
@@ -41,7 +41,7 @@ class PaymentService {
     public void delete(Long id, Customer customer) {
         Payment payment = PaymentRepository.query([id: id, customer: customer]).get()
 
-        if (!payment) throw new BusinessException("Cobrança não encontrada")
+        if (!payment) throw new BusinessException(Utils.getMessageProperty("default.not.found.message.feminine", "Cobrança"))
 
         payment.deleted = true
         payment.save(failOnError: true)
@@ -50,7 +50,7 @@ class PaymentService {
     public void confirm(Long id) {
         Payment payment = PaymentRepository.query([id: id, ignoreCustomer: true]).get()
 
-        if (!payment) throw new BusinessException("Cobrança não encontrada")
+        if (!payment) throw new BusinessException(Utils.getMessageProperty("default.not.found.message.feminine", "Cobrança"))
 
         payment.status = PaymentStatus.RECEIVED
         payment.paymentDate = new Date()
@@ -58,10 +58,10 @@ class PaymentService {
     }
 
     public static void validate(Map params) {
-        if (params.payerCpfCnpj.isBlank()) throw new BusinessException("É preciso selecionar um pagador")
-        if (params.billingType.isBlank()) throw new BusinessException("É preciso selecionar um tipo de recebimento aceito")
-        if (params.dueDate.isBlank()) throw new BusinessException("É preencher o campo data")
-        if (params.value.isBlank()) throw new BusinessException("É preciso preencher o campo valor")
+        if (params.payerCpfCnpj.isBlank()) throw new BusinessException(Utils.getMessageProperty("default.null.message", "CPF / CNPJ"))
+        if (params.billingType.isBlank()) throw new BusinessException(Utils.getMessageProperty("default.null.message", "tipo de recebimento aceito"))
+        if (params.dueDate.isBlank()) throw new BusinessException(Utils.getMessageProperty("default.null.message", "data"))
+        if (params.value.isBlank()) throw new BusinessException(Utils.getMessageProperty("default.null.message", "valor"))
     }
 
     public void setAsOverdue() {
