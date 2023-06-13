6e129fe5ad25978072fe4216a58baf1eb211b8fb
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-13 09:43:28 -0300

    Correção de erros

--- grails-app/controllers/gocharges/PaymentController.groovy
+++ grails-app/controllers/gocharges/PaymentController.groovy
@@ -1,15 +1,16 @@
 package gocharges
 
+import gocharges.controller.base.BaseController
 import gocharges.exception.BusinessException
 import gocharges.payment.PaymentRepository
 import gocharges.payment.adapter.PaymentAdapter
 import shared.FlashMessageType
 
-class PaymentController {
+class PaymentController extends BaseController {
 
     PaymentService paymentService
 
-    public Map index() {
+    public index() {
         List<Payment> payments = paymentService.list()
         Boolean showNewPaymentForm = false
 
@@ -20,10 +21,11 @@ class PaymentController {
         render(view: "index", model: [payments: payments, showNewPaymentForm: showNewPaymentForm])
     }
 
-    public Map save() {
+    public save() {
         try {
+            println(params)
             PaymentAdapter paymentAdapter = new PaymentAdapter(params)
-            Payment payment = paymentService.save(paymentAdapter)
+            Payment payment = paymentService.save(paymentAdapter, getCurrentCustomer())
 
             flash.message = "Cobrança criada com sucesso"
             flash.type = FlashMessageType.SUCCESS
@@ -31,6 +33,7 @@ class PaymentController {
             flash.message = businessException.getMessage()
             flash.type = FlashMessageType.ERROR
         } catch (Exception exception) {
+            println(exception.getMessage())
             flash.message = "Erro inesperado, tente novamente mais tarde"
             flash.type = FlashMessageType.ERROR
             log.info("PaymentController.save >> Erro em criar cobrança com os seguintes dados: ${params}")
@@ -39,19 +42,19 @@ class PaymentController {
         }
     }
 
-    public Map edit() {
+    public edit() {
         Long id = Long.valueOf(params.id)
         Payment payment = PaymentRepository.query([id: id]).get()
 
         render(view: "edit", model: [payment: payment])
     }
 
-    public Map update() {
+    public update() {
         try {
             PaymentAdapter adapter = new PaymentAdapter(params)
             Long id = Long.valueOf(params.id)
 
-            paymentService.update(id, adapter)
+            paymentService.update(id, adapter, getCurrentCustomer())
 
             flash.message = "Cobrança alterada com sucesso"
             flash.type = FlashMessageType.SUCCESS
@@ -67,11 +70,11 @@ class PaymentController {
         }
     }
 
-    public Map showForm() {
+    public showForm() {
         chain(action: "index", model: [showNewPaymentForm: true])
     }
 
-    public Map delete() {
+    public delete() {
         try {
             Long id = Long.valueOf(params.id)
             paymentService.delete(id)
@@ -90,7 +93,7 @@ class PaymentController {
         }
     }
 
-    public Map confirm() {
+    public confirm() {
         try {
             Long id = Long.valueOf(params.id)
             paymentService.confirm(id)
--- grails-app/services/gocharges/PaymentService.groovy
+++ grails-app/services/gocharges/PaymentService.groovy
@@ -10,9 +10,9 @@ import grails.gorm.transactions.Transactional
 @Transactional
 class PaymentService {
 
-    public Payment save(PaymentAdapter adapter) {
+    public Payment save(PaymentAdapter adapter, Customer customer) {
         Payment payment = new Payment()
-        payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj]).get()
+        payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj, customer: customer]).get()
         payment.billingType = adapter.billingType
         payment.dueDate = adapter.dueDate
         payment.value = adapter.value
@@ -25,9 +25,9 @@ class PaymentService {
         return PaymentRepository.query([includeDeleted: false]).list()
     }
 
-    public Payment update(Long id, PaymentAdapter adapter) {
+    public Payment update(Long id, PaymentAdapter adapter, Customer customer) {
         Payment payment = PaymentRepository.query([id: id]).get()
-        payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj]).get()
+        payment.payer = PayerRepository.query([cpfCnpj: adapter.payerCpfCnpj, customer: customer]).get()
         payment.billingType = adapter.billingType
         payment.dueDate = adapter.dueDate
         payment.value = adapter.value
