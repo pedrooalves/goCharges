a461a75be46390953d0c482a1a1f277dad993627
Pedro Alves <pedro.alves@asaas.com.br>
2023-06-19 13:03:40 -0300

    Criação método de formatar valor para BR no Utils

--- grails-app/utils/shared/Utils.groovy
+++ grails-app/utils/shared/Utils.groovy
@@ -2,6 +2,8 @@ package shared
 
 import grails.util.Holders
 
+import java.text.NumberFormat
+
 public class Utils {
 
     static def messageSource = Holders.grailsApplication.mainContext.getBean("messageSource")
@@ -13,5 +15,13 @@ public class Utils {
 
     public static String getMessageProperty(String code, String[] args) {
         messageSource.getMessage(code, args, null)
+    }
+
+    public static getCurrencyWithoutMonetarySimbol(BigDecimal value) {
+        Locale localeBR = new Locale("pt", "BR")
+        NumberFormat numberFormat = NumberFormat.getInstance(localeBR)
+        numberFormat.setMinimumFractionDigits(2)
+
+        return numberFormat.format(value)
     }
 }
