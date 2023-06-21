83e2fb95f7bd21eaaaf113baa35574fdfe536557
Gabriel Ferreira do Prado <gabriel.prado@asaas.com.br>
2023-06-21 16:03:03 -0300

    NotificationType

--- grails-app/domain/gocharges/Notification.groovy
+++ grails-app/domain/gocharges/Notification.groovy
@@ -1,9 +1,11 @@
 package gocharges
 
 import gocharges.domain.base.BaseEntity
+import gocharges.notification.NotificationType
 
 class Notification extends BaseEntity {
 
+    NotificationType notificationType
     Customer customer
     Payment payment
     Boolean unread = true
